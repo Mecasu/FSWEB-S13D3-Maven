08d1c214cc320642d7ed2a7471777a36c2c1dedc
Mecasu <melihcansunay@live.com>
2024-05-22 19:27:34 +0300

    check

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Person {
+    String firstName;
+    String lastName;
+    String grade;
+    int age;
+    int number;
+    int identity;
+    public Person(String firstName, String lastName, int age){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+    }
+    public Person(String firstName, String lastName, int age, String grade, int number, int identity){
+     this(firstName,lastName,age);
+     this.number=number;
+     this.grade=grade;
+     this.identity=identity;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public boolean isTeen(){
+        return age>=13 && age<=19;
+    }
+
+}
---
+++ src/main/java/org/example/Wall.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Wall {
+    double width;
+    double height;
+
+    public Wall(double width,double height){
+        this.width=width;
+        this.height=height;
+    }
+    public double getWidth(){
+        return width;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public void setWidth(double param1){
+        if(param1<0){
+            width=0;
+        }else{
+            width=param1;
+        }
+    }
+    public void setHeight(double param1){
+        if(param1<0){
+            height=0;
+        }else {
+            height=param1;
+        }
+    }
+    public double getArea(){
+        return width*height;
+    }
+}
